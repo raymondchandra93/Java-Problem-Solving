34fac7d8fa75d51286d279d4062062ee4efc7718
raymond.chandra <raymond.chandra@fdmgroup.com>
2025-03-12 18:51:51 -0400

    Adding merge sort

--- src/main/java/com/raymondchandra/intermediate/MergeSort.java
+++ src/main/java/com/raymondchandra/intermediate/MergeSort.java
@@ -4,50 +4,63 @@ import java.util.Arrays;
 
 public class MergeSort {
 	
-	private static void mergeSort(int[] arr, int left, int right) {
-		while(right > left) {
-			int middle = left + (right-left) / 2;
-			
-			mergeSort(arr, left, middle);
-			mergeSort(arr, middle+1, right);
-			
-			int[] newArr = organize(arr, left, middle+1, right);
-		}
-	}
-	
-	private static int[] organize(int[] arr, int left, int middle, int right) {
-		
-		int[] newArr = new int[right-left+1];
-		int i = 0;
-		
-		int l = left;
-		int r = middle;
-		
-		while(l < middle || r < right) {
-			if(arr[left] > arr[middle]) {
-				newArr[i] = arr[r];
-				r++;
-				
-			} else {
-				newArr[i] = arr[l];
-				l++;
-				
-			}
-		}
-		
-		return newArr;
-	}
+	public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
 
-	public static void main(String[] args) {
-		int[] arr = {3, 2, 1, 5, 4, 6};
-		
-		System.out.println("-- Before --");
-		System.out.println(Arrays.toString(arr));
-		
-		mergeSort(arr, 0, arr.length-1);
-		
-		System.out.println("-- After --");
-		System.out.println(Arrays.toString(arr));
-	}
+            // Recursively sort left and right halves
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
 
+            // Merge the sorted halves
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        // Create temporary arrays
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        // Copy data to temporary arrays
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        // Merge the two arrays
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements (if any)
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 3, 8, 5, 2, 7, 4, 1};
+        System.out.println("Original array: " + Arrays.toString(arr));
+
+        mergeSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
 }
